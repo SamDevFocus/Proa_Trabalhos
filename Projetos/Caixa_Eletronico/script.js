40f944ea36ec3a3b1d9eb8a7af2c8ef4209a9e74
Gabriel Franco <g.franco@aluno.ifsp.edu.br>
2023-09-10 00:08:14 -0300

    finalização do projeto

--- Projetos/Caixa_Eletronico/script.js
+++ Projetos/Caixa_Eletronico/script.js
@@ -1,75 +1,154 @@
 var saldo = 100.5; //Variavel Global
-		
-var nome = prompt("digite seu nome");
-alert(`bem vindo ao caixa eletrônico ${nome}`);
+var nomeUsuario;
+var senhaCorreta = '3589';
 
-		function inicio() {
-			//ESCOPO
-			//Variavel Local]
-			
-			
+function inicio() {
 
-			var escolha = parseInt(prompt('Selecione uma opção 1.) Saldo 2.) Depósito 3.) Saque 4.) Sair'));
-				
-			if (escolha === 1) {
-				ver_saldo();
-			} else if (escolha === 2) {
-				fazer_deposito();
-			} else if (escolha === 3) {
-				fazer_saque();
-			} else if (escolha === 4) {
-				sair();
-			} else {
-				erro();
-			}
-		}
+    nomeUsuario = prompt("digite seu nome");
+    alert(`bem vindo ao caixa eletrônico ${nomeUsuario}`);
 
-		function ver_saldo() {
-			alert(`seu saldo é ${saldo} `);
-			inicio();
-		}
+    //ESCOPO
+    //Variavel Local
+    /* var escolha = parseInt(prompt('Selecione uma opção:\n (1-Saldo)\n (2-Depósito) (3-Saque)\n (4-Sair)')); */
+    while (true) {
 
-		function fazer_deposito() {
-			
-			var deposito = parseFloat(prompt('Qual o valor para depósito?'));
-			
-			// Not a Number --> Isso é um não-número?
-			if (isNaN(deposito) || deposito === '') {
-				alert('Por favor, informe um número.');
-				fazer_deposito();
-			} else {
-				saldo += deposito;
-				//saldo = saldo + deposito;
-				ver_saldo();
-			}
-		}
+        var menu = parseInt(prompt(
+            "Escolha uma opção:\n" + 
+            "1. Saldo\n" +
+            "2. Extrato\n" +
+            "3. Saque\n" +
+            "4. Depósito\n" +
+            "5. Transferência\n" +
+            "6. Sair"
+        ));
 
-		function fazer_saque() {
-			
-			var saque = parseFloat(prompt('Qual o valor para saque?'));
-			
-			if (isNaN(saque) || saque === '') {
-				alert('Por favor, informe um número.');
-				fazer_saque();
-			} else {
-				saldo -= saque;
-				//saldo = saldo - saque;
-				ver_saldo();
-			}
-		}
 
-		function erro() {
-			alert('Por favor, informe um número entre 1 e 4.');
-			inicio();
-		}
+        switch(menu){
+            case 1: 
+                ver_saldo();
+                break;
+            case 2:
+                ver_extrato();
+                break;
+            case 3: 
+                fazer_saque();
+                break;
+            case 4:
+                fazer_deposito();
+                break;
+            case 5: 
+                fazer_transferencia();
+                break;
+            case 6:
+                sair();
+                break;
+            default:
+                erro();
+                
+            }
+        }
+    }   
+            
+   
+function ver_saldo() {
+    /*senha */
+    var senha = prompt("Digite sua senha:");
+    if (senha !== senhaCorreta) {
+        alert("Senha incorreta. Tente novamente.");
+        return; // Saia da função em caso de senha incorreta
+    }
 
-		function sair() {
-			var confirma = confirm('Você deseja sair?');
-			if (confirma) {
-				window.close();
-			} else {
-				inicio();
-			}
-		}
+    alert(`Seu saldo é R$${saldo.toFixed(2)}`);
+}
 
-		inicio();
+// Funcionalidade para rastrear transações e exibir o extrato
+var extrato = [];
+
+function ver_extrato() {
+    /*senha */
+    senha = prompt("Digite sua senha:");
+    if (senha !== senhaCorreta) {
+        alert("Senha incorreta. Tente novamente.");
+        return; // Saia da função em caso de senha incorreta
+    }
+
+    if (extrato.length === 0) {
+        alert("Extrato:\nNenhuma transação encontrada.");
+    } else {
+        alert("Extrato:\n" + extrato.join("\n"));
+    }
+}
+
+function fazer_saque() {
+    /*senha */
+
+    senha = prompt("Digite sua senha:");
+    if (senha !== senhaCorreta) {
+        alert("Senha incorreta. Tente novamente.");
+        return; // Saia da função em caso de senha incorreta
+    }
+
+    var saque = parseFloat(prompt('Qual o valor para saque?'));
+    if (isNaN(saque) || saque <= 0 || saque > saldo) {
+        alert('Operação não autorizada. Verifique o valor do saque.');
+        return; // Saia da função em caso de valor inválido
+    }
+
+    saldo -= saque;
+    extrato.push(`Saque: - R$${saque.toFixed(2)}`);
+    alert('Saque realizado com sucesso.');
+}
+
+function fazer_deposito() {
+    var deposito = parseFloat(prompt('Qual o valor para depósito?'));
+    if (isNaN(deposito) || deposito <= 0) {
+        alert('Operação não autorizada. Verifique o valor do depósito.');
+        return; // Saia da função em caso de valor inválido
+    }
+
+    saldo += deposito;
+    extrato.push(`Depósito: + R$${deposito.toFixed(2)}`);
+    alert('Depósito realizado com sucesso.');
+}
+
+function fazer_transferencia() {
+    
+    senha = prompt("Digite sua senha:");
+    if (senha !== senhaCorreta) {
+        alert("Senha incorreta. Tente novamente.");
+        return; // Saia da função em caso de senha incorreta
+    }
+
+    var valorTransferencia = parseFloat(prompt('Qual o valor para transferência?'));
+    if (isNaN(valorTransferencia) || valorTransferencia <= 0 || valorTransferencia > saldo) {
+        alert('Operação não autorizada. Verifique o valor da transferência.');
+        return; // Saia da função em caso de valor inválido
+    }
+
+    var contaDestino = prompt('Digite o número da conta de destino:');
+    if (isNaN(contaDestino)) {
+        alert('Operação não autorizada. Verifique o número de conta de destino.');
+        return; // Saia da função em caso de valor inválido
+    }
+
+    extrato.push(`Transferência para conta ${contaDestino}: - R$${valorTransferencia.toFixed(2)}`);
+    saldo -= valorTransferencia;
+    /* saldo -= valorTransferencia; */
+    alert('Transferência realizada com sucesso.');
+}
+
+function erro() {
+    alert('Por favor, informe um número entre 1 e 6.');
+}
+
+function sair() {
+    alert(`Obrigado por utilizar nossos serviços, ${nomeUsuario}, foi um prazer ter você por aqui!`);
+    window.close();
+   
+}
+
+inicio();
+
+
+
+
